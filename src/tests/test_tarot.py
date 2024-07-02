8b0e1c87773ff772fcd3979281932c464defa859
Matheus Pinheiro <17935077+mp-pinheiro@users.noreply.github.com>
2024-07-02 10:57:55 -0300

    Remove old tests

--- src/tests/test_tarot.py
+++ src/tests/test_tarot.py
@@ -8,25 +8,25 @@ def test_version():
     assert __version__ == "0.1.0"
 
 
-def test_tarot_new_user():
-    tarot = Tarot()
+# def test_tarot_new_user():
+#     tarot = Tarot()
 
-    update = Mock()
-    update.message.from_user.id = "123"
-    update.message.from_user.username = "test_user"
-    update.message.from_user.full_name = "Test User"
-    update.message.chat.id = "456"
+#     update = Mock()
+#     update.message.from_user.id = "123"
+#     update.message.from_user.username = "test_user"
+#     update.message.from_user.full_name = "Test User"
+#     update.message.chat.id = "456"
 
-    context = Mock()
-    context.bot.send_message = Mock()
-    context.args = []  # assume daily command
+#     context = Mock()
+#     context.bot.send_message = Mock()
+#     context.args = []  # assume daily command
 
-    tarot._process(update, context)
+#     tarot._process(update, context)
 
-    user = tarot._users["123"]
-    assert user["display_name"] == "test_user"
-    assert user["request_date"]
-    assert user["card"]
+#     user = tarot._users["123"]
+#     assert user["display_name"] == "test_user"
+#     assert user["request_date"]
+#     assert user["card"]
 
 
 # def test_tarot_existing_user():
